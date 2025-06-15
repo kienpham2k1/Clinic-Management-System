86976c68eb8e01619a2f6a980132a6f4595165a4
KienPT <kienpt.dev.01@gmail.com>
2025-06-15 17:27:08 +0700

    sample layout

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,28 +1,5 @@
-// src/features/auth/authSlice.ts
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { UserProfile } from '../../types/user';
-// Giả lập API login
-export const login = createAsyncThunk(
-  'auth/login',
-  async (credentials: { username: string; password: string }, thunkAPI) => {
-    try {
-      // Giả lập API call
-      const response = await new Promise((resolve) =>
-        setTimeout(
-          () =>
-            resolve({
-              user: { id: 1, firstName: 'Dr. Kien 2', role: 'doctor m' },
-              token: 'fake-jwt-token-123',
-            }),
-          1000
-        )
-      );
-      return response as { user: UserProfile; token: string };
-    } catch (error) {
-      return thunkAPI.rejectWithValue('Login failed');
-    }
-  }
-);
 interface AuthState {
   user: null | UserProfile,
   token: null | string,
@@ -49,23 +26,23 @@ const authSlice = createSlice({
       state.isAuthenticated = false;
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(login.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isAuthenticated = true;
-        state.loading = false;
-      })
-      .addCase(login.rejected, (state, action) => {
-        state.error = action.payload as string;
-        state.loading = false;
-      });
-  },
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(login.pending, (state) => {
+  //       state.loading = true;
+  //       state.error = null;
+  //     })
+  //     .addCase(login.fulfilled, (state, action) => {
+  //       state.user = action.payload.user;
+  //       state.token = action.payload.token;
+  //       state.isAuthenticated = true;
+  //       state.loading = false;
+  //     })
+  //     .addCase(login.rejected, (state, action) => {
+  //       state.error = action.payload as string;
+  //       state.loading = false;
+  //     });
+  // },
 });
 
 export const { logout } = authSlice.actions;
